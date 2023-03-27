8bcdc94788f36db7d38b854dce1f953acb6982ac
timyan56 <t.yan@sleekflow.io>
2023-03-27 11:51:28 +0800

    update

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,9 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            //"to": "{{Contact.Attribute.tim_testing.Phone}}"
-            "to": "{{Contact.Default.MobilePhone}}"
-            //"emailAddress": "{{Contact.Default.EmailAddress}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
